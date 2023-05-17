ad18834b4bc8be924627c7726f428dd6e135a086
Maspers <matthewbrian35@gmail.com>
2023-05-16 18:45:53 -0700

    final commit

--- EmployeeSQL/table_creation.sql
+++ EmployeeSQL/table_creation.sql
@@ -10,24 +10,25 @@ DROP TABLE IF EXISTS salaries;
 -- tables
 
 CREATE TABLE departments (
-    dept_no VARCHAR PRIMARY KEY NOT NULL,
-    dept_name VARCHAR NOT NULL
+    dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(30) NOT NULL
 );
 
 
 CREATE TABLE titles (
-    title_id VARCHAR PRIMARY KEY NOT NULL,
-    title VARCHAR NOT NULL
+    title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+    title VARCHAR NOT(30) NULL
 );
 
+-- SELECT Max(CHAR_LENGTH(title)) FROM titles;
 
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
@@ -35,7 +36,7 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
 	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -43,7 +44,7 @@ CREATE TABLE dept_emp (
 
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
 	PRIMARY KEY (dept_no, emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
